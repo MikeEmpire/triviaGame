7bec35d27e05010ceab94eab23534b8523f61767
Mike <aolie94@gmail.com>
2017-02-27 17:44:11 -0800

    made curren theme. Made some javascript functions to generate random colors. Will change trivia theme for Vans Off the Wall based questions for trivia

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-    $("#start").click(function() {
+    function changeColor() {
+        var colors = ['red', 'blue', 'green', 'grey'],
+            color, randColor;
+        do {
+            randColor = colors[Math.floor(Math.random() * colors.length)];
+        } while (color == randColor);
+        $('h1, p').css('color', randColor);
+        color = randColor;
+    }
 
+    setTimeout(changeColor, 1000);
+
+    $("#startButton").click(function() {
+        var selectedAnswer, answers, question, questionBank, selectedQuestion, promptQuestion;
+        promptQuestion = $("<h1>Here is a new question</h1>")
+        $("#questionBox").html(promptQuestion);
     });
 });
